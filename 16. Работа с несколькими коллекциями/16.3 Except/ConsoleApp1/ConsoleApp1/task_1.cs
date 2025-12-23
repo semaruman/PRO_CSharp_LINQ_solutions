756095722af30dd41c2e59ddc8fb80968c4db8ef
Semen <semen.ruman@gmail.com>
2025-12-23 18:29:08 +0300

    Сделал все задания в разделе Работа с несколькими коллекциями -> Except

---
+++ 16. Работа с несколькими коллекциями/16.3 Except/ConsoleApp1/ConsoleApp1/task_1.cs
@@ -0,0 +1,16 @@
+﻿static class Ex
+{
+    static IEnumerable<int> MyExcept(this IEnumerable<int> items1, IEnumerable<int> items2)
+    {
+        // реализовать логику
+        HashSet<int> items2set = new HashSet<int>(items2);
+
+        foreach (var item in items1)
+        {
+            if (items2set.Add(item))
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ 16. Работа с несколькими коллекциями/16.3 Except/ConsoleApp1/ConsoleApp1/task_3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+public class MainClass3
+{
+    public static void Main()
+    {
+        // put your c# code here
+        int[] numbers1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] numbers2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int[] numbers3 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Console.Write(string.Join(" ", numbers1.Except(numbers2))+ " ");
+
+        Console.WriteLine(string.Join(" ", numbers3.Except(numbers2).Except(numbers1)));
+    }
+}
