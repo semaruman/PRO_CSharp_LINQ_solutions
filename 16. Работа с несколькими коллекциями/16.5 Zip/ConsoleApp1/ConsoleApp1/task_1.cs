e3f69d9e0cf8b7860382c7ab9c4f2fd7805a225b
Semen <Semen@HOME-PC>
2025-12-26 21:46:15 +0300

    Сделал часть заданий в разделе с Zip

---
+++ 16. Работа с несколькими коллекциями/16.5 Zip/ConsoleApp1/ConsoleApp1/task_1.cs
@@ -0,0 +1,13 @@
+﻿static class Ex
+{
+    static IEnumerable<int> MyZip(this IEnumerable<int> items1, IEnumerable<int> items2)
+    {
+        // реализовать логику
+        int newSize = Math.Min(items1.Count(), items2.Count());
+
+        for (int i = 0; i < newSize; i++)
+        {
+            yield return items1.ElementAt(i) + items2.ElementAt(i);
+        }
+    }
+}
